bd62cb0ebe451f5ceae87bfb091a2250d9df47d9
Andrew Fawcett <afawcett@afawcett-ltm.internal.salesforce.com>
2018-11-19 19:29:53 -0800

    Implemented batchable errors retry component

--- force-brf/main/aura/brf_batchableErrors/brf_batchableErrorsController.js
+++ force-brf/main/aura/brf_batchableErrors/brf_batchableErrorsController.js
@@ -1,5 +1,68 @@
 ({
-    myAction : function(component, event, helper) {
+    handleRowAction : function(cmp, event, helper) {
+        var action = event.getParam('action');
+        var row = event.getParam('row');
+        switch (action.name) {
+            case 'retry':
+                var action = cmp.get("c.retryJob");
+                action.setParams({ retryJobId : row.JobId__c });
+                action.setCallback(this, function(response) {
+                    var state = response.getState();
+                    if (state === "SUCCESS") {
+                        cmp.set('v.failedJobs', response.getReturnValue());
+                        var resultsToast = $A.get("e.force:showToast");
+                        resultsToast.setParams({ "message": "Job retry for " + row.JobApexClass__c + " has been submitted."});
+                        resultsToast.fire();
+                    }
+                    else if (state === "INCOMPLETE") {
+                    }
+                    else if (state === "ERROR") {
+                        var errors = response.getError();
+                        if (errors) {
+                            if (errors[0] && errors[0].message) {
+                                console.log("Error message: " + errors[0].message);
+                            }
+                        } else {
+                            console.log("Unknown error");
+                        }
+                    }
+                });
+                $A.enqueueAction(action);    
+                break;
+        }        
+    },
+    doInit : function(cmp, event, helper) {
+        var actions = [
+            { label: 'Retry', name: 'retry' }
+        ];
+        // Configure data table component columns
+        cmp.set('v.columns', [
+            { label: 'Job Apex Class', fieldName: 'JobApexClass__c', type: 'text'},
+            { label: 'Job Creation Date', fieldName: 'JobCreatedDate__c', type: 'date'},
+            { label: 'Job Errors', fieldName: 'JobErrors__c', type: 'number'},
+            { type: 'action', typeAttributes: { rowActions: actions } }
+        ]);        
 
+        // Retrieve failed jobs from server
+        var action = cmp.get("c.failedJobs");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                cmp.set('v.failedJobs', response.getReturnValue());
+            }
+            else if (state === "INCOMPLETE") {
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
